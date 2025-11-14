69d0040c943554022acb8a0ce45faca5a53bc959
Kxress <gymystik@gmail.com>
2025-11-14 13:40:15 +0100

    Minor tweaks
    Works for only one face at a time now, changed face rectangle color + minor improvement

--- main.py
+++ main.py
@@ -20,41 +20,51 @@ for file in os.listdir("./faces"):
 
 face_locations = []
 face_encodings = []
-process_this_frame = False
-
 face_names = []
+
+process_this_frame = True
+
+rect_color = (0, 255, 255)
 
 while True:
     ret, frame = video_capture.read()
-
-    if ret and frame is not None:
-        process_this_frame = True
+    if not ret:
+        continue
 
     if process_this_frame:
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(frame, None, fx=0.25, fy=0.25)
 
-        rgb_small_frame = np.ascontiguousarray(small_frame)
-        
+        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        face_encodings_len = len(face_encodings)
+
+        if face_encodings_len == 1:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encodings[0])
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encodings[0])
+
+            best_match_index = np.argmin(face_distances)
             name = "Unknown"
 
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
 
-            if len(face_names) >= 5 and len(set(face_names[-5:])) == 1:
-                print("Ostatnie 5 zarejestrowany twarzy to: " + face_names[-1])
+            if len(face_names) > 5:
+                face_names = face_names[-5:]
+
+            if len(face_names) == 5 and len(set(face_names)) == 1:
+                rect_color = (0, 255, 0)
+            else:
+                rect_color = (0, 255, 255)
 
     process_this_frame = not process_this_frame
 
-    if face_locations and face_names:
+
+    if face_locations and face_names and face_encodings_len == 1:
         (top, right, bottom, left), name = face_locations[-1], face_names[-1]
 
         top *= 4
@@ -62,9 +72,11 @@ while True:
         bottom *= 4
         left *= 4
 
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(frame, (left, bottom), (right, bottom - 35), (0, 0, 255), cv2.FILLED)
+        cv2.rectangle(frame, (left, top), (right, bottom), rect_color, 2)
+        cv2.rectangle(frame, (left, bottom), (right, bottom - 35), rect_color, cv2.FILLED)
         cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+    elif face_encodings_len > 1:
+        cv2.putText(frame, "WYKRYTO WIECEJ NIZ JEDNA TWARZ", (30, 450), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 255), 2)
 
     cv2.imshow('Students Surveillance System', frame)
 
