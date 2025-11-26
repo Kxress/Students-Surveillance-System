661e93ed0a9b696fa14fbce29190195704fd102f
Kxress <gymystik@gmail.com>
2025-11-26 18:20:00 +0100

    Added new stuff to face confirming

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import face_recognition
 import cv2
 import numpy as np
 import os
-import messagebox
+from tkinter import simpledialog, messagebox
+import datetime
+import sys
 
 video_capture = cv2.VideoCapture(0)
 
@@ -26,6 +28,12 @@ face_names = []
 process_this_frame = True
 rect_color = (0, 255, 255)
 
+period_times = [datetime.time(8, 0), datetime.time(8, 50), datetime.time(9, 45), datetime.time(10, 50), datetime.time(11, 40), datetime.time(12, 35), datetime.time(13, 25), datetime.time(14, 15)]
+days_pl = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"]
+
+present = []
+late = []
+
 def confirm_face():
     global face_names
 
@@ -34,12 +42,29 @@ def confirm_face():
 
         if len(set(face_names)) == 1:
             if face_names[0] != "Unknown":
-                if messagebox.askyesno("Wykryto twarz", f"Wykryto twarz: {face_names[0]}. Czy twarz się zgadza?"):
-                    messagebox.showinfo("Obecność zatwierdzona", "Zatwierdzono obecność! Kliknij OK by zeskanować następną twarz.")
+                current_datetime = datetime.datetime.now()
+                current_time = current_datetime.time()
+                current_period = 0
+
+                for i in range(len(period_times)):
+                    if current_time < period_times[i]:
+                        current_period = i + 1
+                        break
+
+                if face_names[0] not in present and face_names[0] not in late:
+                    present.append(face_names[0]) if datetime.timedelta(hours=current_time.hour, minutes=current_time.minute) - datetime.timedelta(hours=period_times[current_period].hour, minutes=period_times[current_period].minute) <= datetime.timedelta(minutes=15) else late.append(face_names[0])
+
+                with open(f"{current_datetime.strftime('%d.%m')} ({days_pl[current_datetime.weekday()]}) - Lekcja {current_period} - {grade}.txt", "w") as file:
+                    file.write("Obecni:\n\n" + "\n".join(present) + "\n\n\nSpóźnienia:\n\n" + "\n".join(late))
             else:
                 messagebox.askyesno("Nieznana twarz", "Nie wykryto twarzy. Być może nie istnieje ona w bazie danych. Czy dodać twarz do bazy?")
 
             face_names.clear()
+
+grade = simpledialog.askstring("Klasa", "Podaj nazwę klasy:")
+
+if grade == "":
+    sys.exit()
 
 while True:
     ret, frame = video_capture.read()
@@ -57,7 +82,7 @@ while True:
         face_encodings_len = len(face_encodings)
 
         if face_encodings_len == 1:
-            matches = face_recognition.compare_faces(known_face_encodings, face_encodings[0])
+            matches = face_recognition.compare_faces(known_face_encodings, face_encodings[0], 0.5)
             face_distances = face_recognition.face_distance(known_face_encodings, face_encodings[0])
 
             best_match_index = np.argmin(face_distances)
